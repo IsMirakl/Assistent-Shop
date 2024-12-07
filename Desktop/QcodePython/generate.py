84a96502de7cde1c3b33f3119228b05d87219eb9
IsMirakl <135332620+IsMirakl@users.noreply.github.com>
2024-12-07 15:58:38 +0300

    Delete Desktop/QcodePython directory

--- Desktop/QcodePython/generate.py
+++
@@ -1,5 +0,0 @@
-import qrcode
-
-img = qrcode.make("https://t.me/danyafrontender")
-type(img)
-img.save("qrcode.png")
--- Desktop/QcodePython/reader.py
+++
@@ -1,6 +0,0 @@
-import cv2
-
-img = cv2.imread("qrcode.png")
-detect = cv2.QRCodeDetector()
-value, _, _ = detect.detectAndDecode(img)
-print(value)
